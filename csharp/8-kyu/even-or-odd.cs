8954d40593781593c87fa21e7723c1739388c516
Tricia Amheiser <tricia@taarts.net>
2022-05-01 23:05:38 -0400

    Even or Odd

---
+++ csharp/8-kyu/even-or-odd.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Solution
+{
+    public class SolutionClass
+    {
+        public static string EvenOrOdd(int number)
+        {
+            // if the number modulus 2, is equal to 0, 
+            if (number % 2 = 0)
+            {
+                //the number is even
+                return "Even";
+            }
+            {
+                return "Odd";
+                // else the number is odd
+            }
+        }
+    }
+}
